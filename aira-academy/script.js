6124aeb6cd53c38bd2be17f45903670ce7b6ba59
Dhamodhar Kamini <dhamukamini92081@gmail.com>
2025-03-12 12:24:47 +0530

    commit

--- aira-academy/script.js
+++ aira-academy/script.js
@@ -8,3 +8,33 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     
 });
+
+// function toggleFranchisecontainer(){
+//     window.location.href = "franchise.html";
+//     window.open("franchise.html", "_blank");
+// }
+
+// function navgateToContact(){
+//     window.location.href = "index.html#contact";
+// }
+
+
+// franchise.html navigate section
+// function toggleFranchisecontainer() {
+//     if (window.matchMedia("(max-width: 700px)").matches) {
+//         // If the screen width is 600px or less, open in a new tab
+//         window.open("franchise.html", "_blank");
+//     } else {
+//         // Otherwise, redirect in the same tab
+//         window.location.href = "franchise.html";
+//     }
+// }
+
+// document.querySelector(".partnerbtn").addEventListener("click", toggleFranchisecontainer);
+// document.querySelector(".partnerbtn").addEventListener("touchstart", toggleFranchisecontainer);
+
+// document.querySelector(".requestBtn").addEventListener("click", navgateToContact);
+// document.querySelector(".requestBtn").addEventListener("touchstart", navgateToContact);
+
+
+ 
